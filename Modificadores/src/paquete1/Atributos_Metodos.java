0ffbeb27cd17e7fdcffee8ea013a83ca8b607fcc
Monsivais14 <monsivaisalfredo2@gmail.com>
2021-09-20 17:25:08 -0500

    v1.1

---
+++ Modificadores/src/paquete1/Atributos_Metodos.java
@@ -0,0 +1,14 @@
+
+package paquete1;
+
+
+public class Atributos_Metodos {
+    
+    protected int valorUno;
+    protected int valorDos;
+    protected String ValorTres;
+    
+    protected void metodo1(){
+    }
+    
+}
---
+++ Modificadores/src/paquete1/ClaseJava1.java
@@ -0,0 +1,19 @@
+
+package paquete1;
+
+
+public class ClaseJava1 {
+    public static void main(String []args){
+       
+    
+        Atributos_Metodos instancia = new Atributos_Metodos();
+            /*
+            objeto con el que se comunica ocn la otra 
+            */
+            /*
+            instancia.set_v1(13);
+            System.out.println(instancia.get_v1());
+            */
+            System.out.print(instancia.valorUno);
+    }
+}
