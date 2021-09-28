f6b5245a11842c42b65f77126ad92be8b26cb9ea
Monsivais14 <monsivaisalfredo2@gmail.com>
2021-09-27 22:29:02 -0500

    v1.6

---
+++ ClaseMath/src/ejemplos/constantes.java
@@ -0,0 +1,10 @@
+
+package ejemplos;
+
+public class constantes {
+    public static void main(String []args){
+        
+        System.out.println("El numero e: "+Math.E);
+        System.out.println("El numero pi: "+Math.PI);
+    }
+}
---
+++ Constructor/src/clases/ClaseMain.java
@@ -0,0 +1,13 @@
+package clases;
+
+public class ClaseMain {
+
+    public static void main(String[] args) {
+        /*
+        SinConstructor sin = new SinConstructor();
+        sin.Nombre();
+        sin.Imprimir();
+        */
+        ConConstructor con = new ConConstructor();
+    }
+}
