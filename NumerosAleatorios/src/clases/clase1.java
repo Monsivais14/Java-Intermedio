c730e6e708ec6832ee1c708ad841239e56b6dd3a
Monsivais14 <monsivaisalfredo2@gmail.com>
2021-09-28 23:00:01 -0500

    v1.7

---
+++ NumerosAleatorios/src/clases/clase1.java
@@ -0,0 +1,11 @@
+
+package clases;
+
+public class clase1 {
+   public static void main(String []args){
+       int aleatorio = 0;
+       
+       aleatorio = (int) (Math.random()*10);//el 100 indica el rango limite
+       System.out.print(aleatorio);
+   } 
+}
