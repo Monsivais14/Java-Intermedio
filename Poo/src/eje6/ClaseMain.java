3e2200bb29e5d0122e3b977358c5d9c8ecbaf885
Monsivais14 <88120926+Monsivais14@users.noreply.github.com>
2021-11-08 22:07:32 -0600

    v2.7

---
+++ Poo/src/eje6/ClaseMain.java
@@ -0,0 +1,11 @@
+
+package eje6;
+
+public class ClaseMain {
+    
+    public static void main(String[] args) {
+        
+        Cajero cajero = new Cajero(500);
+        cajero.bienvenida();
+    }
+}
